56b088bf3b4fdc50d653ea5a7bf2fc6029557366
quinaiton <quinaiton@hotmail.com>
2021-05-04 16:29:12 -0700

    Pulls Header Title into Own component

---
+++ client/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Styles from '../Styles/Header.module.scss';
+const Header = () => {
+  return (
+    <div className={Styles.container}>
+      <h1>Crafty Maps</h1>
+      <p> Your thirst Ends here</p>
+    </div>
+  );
+};
+
+export default Header;
