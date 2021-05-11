81b0275dbec2876c1d0046c602f6f20f6afd665c
quinaiton <quinaiton@hotmail.com>
2021-05-11 16:01:37 -0700

    Adds Loading Component

---
+++ client/src/Components/Loading.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Styles from '../Styles/Loading.module.scss';
+import { SiOpenstreetmap } from 'react-icons/si';
+const Loading = () => {
+  return (
+    <div className={Styles.Loading}>
+      <div className={Styles.noFreezeSpinner}>
+        <div id={Styles.noFreezeSpinner}>
+          <div>
+            <i>
+              <SiOpenstreetmap />
+            </i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
