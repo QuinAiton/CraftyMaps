987d5e718ce446c50805ad895224bbe61eb54443
quinaiton <quinaiton@hotmail.com>
2021-05-11 16:00:45 -0700

    Fixes Brewewry Slider and Adds Open Close Button

--- client/src/Components/Breweries.jsx
+++ client/src/Components/Breweries.jsx
@@ -1,29 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useStore from '../store';
 import CardItem from './CardItem';
 import Styles from '../Styles/Breweries.module.scss';
 import { BsPlusCircleFill } from 'react-icons/bs';
+import { FaMinus } from 'react-icons/fa';
+const Breweries = () => {
+  const [open, setOpen] = useState(false);
 
-const Cards = () => {
-  const breweries = useStore((state) => state.breweries);
+  const { breweries, selectedRoute, setSelectedRoute } = useStore();
+
+  //Adds Breweries to Optimized Route Filters out Duplicates
+  const addRouteHandler = (id) => {
+    const selectedBrewery = breweries.find((pub) => pub.id === id);
+    if (!selectedRoute.find((pub) => pub.id === id)) {
+      setSelectedRoute([...selectedRoute, selectedBrewery]);
+    }
+  };
 
   const breweryCards = breweries.map((pub) => {
     return (
       <CardItem
         key={pub.id}
+        id={pub.id}
         name={pub.name}
-        location={(pub.location.address, pub.location.crossStreet)}
+        location={pub.location.join(', ')}
+        category={pub.category}
+        addRouteHandler={addRouteHandler}
       />
     );
   });
   return (
     <div className={Styles.container}>
-      <div className={Styles.breweryToggle}>
-        <BsPlusCircleFill className={Styles.open} />
+      <div className={Styles.breweryToggle} onClick={() => setOpen(!open)}>
+        {open ? (
+          <FaMinus className={Styles.toggle} />
+        ) : (
+          <BsPlusCircleFill className={Styles.toggle} />
+        )}
         <input type='checkbox' />
         <ul className={Styles.breweries}>{breweryCards}</ul>
       </div>
     </div>
   );
 };
-export default Cards;
+export default Breweries;
--- client/src/Components/CardItem.jsx
+++ client/src/Components/CardItem.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Styles from '../Styles/CardItem.module.scss';
-const CardItem = ({ name, location }) => {
+import useStore from '../store';
+const CardItem = ({ name, location, category, id, addRouteHandler }) => {
   return (
     <div className={Styles.container}>
-      <h1>{name}</h1>
+      <h4>{name}</h4>
+      <p>{category}</p>
       <p>{location}</p>
+      <button onClick={() => addRouteHandler(id)}>Add to Route</button>
     </div>
   );
 };
